8ea3faf2d45cc2b2acf251e2025a022e735fadb1
Eroglu98 <58264080+Eroglu98@users.noreply.github.com>
2020-05-12 17:56:53 +0200

    Deteled the Comment section

--- Python Codes/Python SQL Connection.py
+++ Python Codes/Python SQL Connection.py
@@ -2,42 +2,13 @@
 import psycopg2
 from datetime import datetime
 
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(host="localhost", database="Project3", user="postgres", password="fay42fwv")
-
-        # create a cursor
-        cur = conn.cursor()
-
-        # execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-
-        # close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
-
-
 def insert_projectdata(message):
     """ insert a new vendor into the vendors table """
     sql = """I"""
 
     query = "INSERT INTO PROJECTDATA (gpsx, gpsy, humidity, temperature, lighting, ts) VALUES (%s, %s, %s, %s, %s, %s);"
     query2 = "DELETE FROM PROJECTDATA"
-    data =  (12345, 1234, 1, 5, 10123123, datetime.now())
+    data =  (12345, 1234, 1, 43345, 10123123, datetime.now())
 
     conn = None
     try:
