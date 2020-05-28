46fe14a0959fd3a9d4e915eecd3fde37e2e5687c
GeraldoWItcher <64520890+GeraldoWItcher@users.noreply.github.com>
2020-05-28 16:35:15 +0200

    Update MQTTimplementation.ino

--- MQTTimplementation/MQTTimplementation.ino
+++ MQTTimplementation/MQTTimplementation.ino
@@ -4,11 +4,11 @@
 #include <SoftwareSerial.h>
 #include <DHT.h>;
 
-#define DHTPIN 4 //D2
-#define DHTTYPE DHT22
+#define DHTPIN 4 //D2 pin on the ESP
+#define DHTTYPE DHT22 //change in accordance to your sensor
 #ifndef STASSID
-#define STASSID "SSID"
-#define STAPSK "Password"
+#define STASSID "SSID" //change to match yours
+#define STAPSK "PASSWORD"
 #endif
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -16,20 +16,22 @@ TinyGPSPlus gps;
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
-const char* mqtt_broker = "test.mosquitto.org";
+const char* mqtt_broker = "mqtt.eclipse.org"; //change to match your broker
 
-static const int RXPin = 13, TXPin = 15 ;
+static const int RXPin = 13, TXPin = 15 ; // D8 & D7 on the ESP
 static const uint32_t GPSBaud = 9600;
 SoftwareSerial ss(RXPin, TXPin);
 
 float lumValue = 0;
-
 WiFiClient espClient;
 PubSubClient mqttClient(espClient);
 long lastMsg = 0;
 char msg[100];
+int sleeptime;
 
+//behaviour on startup
 void setup(void) {
+  //initializing sensors and wifi
   Serial.begin(9600);
   dht.begin();
   WiFi.mode(WIFI_STA);
@@ -53,20 +55,6 @@ void setup(void) {
   mqttClient.setCallback(callback);
 }
 
-  void callback(char* topic, byte* payload, unsigned int length) {
-    Serial.print("Topic: [");
-    Serial.print(topic);
-    Serial.print("]");
-    Serial.print(" payload:");
-    for (int i=0;i<length; i++){
-      Serial.print((char)payload[i]);
-    }
-    if ((char)payload[0] == '1') {
-      Serial.println(" correct");
-    }else{
-      Serial.println(" wrong");
-    }
-  }
  
 void reconnect(){
   while(!mqttClient.connected()){
@@ -76,15 +64,40 @@ void reconnect(){
 
     if(mqttClient.connect(clientID.c_str())){
         Serial.println("connected ");
-        mqttClient.subscribe("UniqueID");
+        mqttClient.subscribe("IDDeniss/Device1");
     }else{
       Serial.print("Failed, rc=");
       Serial.print(mqttClient.state());
-      Serial.println(" retrying...");
+      Serial.println("retrying...");
       delay(5000);
     }
   }
 }
+
+ //Function for handling incoming MQTT payload
+  void callback(char* topic, byte* payload, unsigned int length) {
+    Serial.print("Topic: [");
+    Serial.print(topic);
+    Serial.print("]");
+    Serial.print(" payload:");
+    for (int i=0;i<length; i++){
+      Serial.print((char)payload[i]);
+    }
+    if ((char)payload[0] == 'S') {
+      Serial.println("Entering manual sleep mode..");
+      sleeptime = (int)payload[1] * 36e8; //example: payload S2 = enter sleep mode for 2 hours
+      ESP.deepSleep(sleeptime); 
+      ;
+    }
+  }
+
+int calculateLum(float Value){
+  float realVolts = Value*3.3/1024;
+  float LDResistance = (3.3-realVolts)*10/realVolts;
+  int LUX = 500/LDResistance;
+  return LUX;
+}
+
 void loop(void) {
   if(!mqttClient.connected()){
     reconnect();
@@ -94,18 +107,19 @@ void loop(void) {
       gps.encode(ss.read());
     } 
   long now = millis();
-  if(now-lastMsg > 5000){
+  if(now-lastMsg > 3e5){ //every 5 minutes, adjust as needed
     lastMsg = now;
-    lumValue = analogRead(A0);
-    if(lumValue < 100){
+    float sensorValue = analogRead(A0);
+    lumValue = calculateLum(sensorValue);
+    if(lumValue < 50){
       Serial.println("no light detected, entering power saving mode");
-      ESP.deepSleep(30e6);
+      ESP.deepSleep(12e8); //20 minutes in microseconds
     }else{
       float hum = dht.readHumidity();
       float temp = dht.readTemperature();
       float xcoor = gps.location.lng();
       float ycoor = gps.location.lat();
-      snprintf (msg,100, "T%f,H%f,X%f,Y%f,L%f",temp,hum,xcoor,ycoor,lumValue);
+      snprintf (msg,100, "T%f,H%f,X%f,Y%f,L%f",temp,hum,xcoor,ycoor,lumValue); //saving into one data string
       mqttClient.publish("IDDeniss/esptest",msg);
     }
   }
